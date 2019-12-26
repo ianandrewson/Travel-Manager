c44c40b81b0c3cb3c9f90012c32bb3bd9fc22970
Ian Andrewson <ianandrewson@gmail.com>
2019-12-26 12:33:01 -0800

    Trip schema implemented, tests pass; need to make virtual field for itenerary items

---
+++ __tests__/trip.test.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Trip = require('../lib/models/Trip.js');
+
+describe('trip schema tests', () => {
+  it('has a required name field', () => {
+    const trip = new Trip();
+    const { errors } = trip.validateSync();
+    expect(errors.name.message).toEqual('Path `name` is required.');
+  });
+  it('can create a new trip', () => {
+    const trip = new Trip({
+      name: 'Trip to Germany 2020'
+    });
+    expect(trip.toJSON()).toEqual({
+      _id: expect.any(mongoose.Types.ObjectId),
+      name: 'Trip to Germany 2020'
+    });
+  });
+});
+
+// beforeAll(() => {
+//   connect();
+// });
+// beforeEach(() => {
+//   return mongoose.connection.dropDatabase();
+// });
+// afterAll(() => {
+//   mongoose.connection.close();
+// });
